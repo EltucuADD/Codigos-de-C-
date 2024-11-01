bb9024221819388e3911cad24982b57c5670ec98
Andres Grau <119760160+EltucuADD@users.noreply.github.com>
2024-11-01 01:01:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero, Resultado = 0;
+
+            Console.WriteLine("Ingrese un numero entero");
+            Numero = Convert.ToInt32(Console.ReadLine());
+
+            Resultado = Numero * 2;
+
+            Console.WriteLine("El resultado es: {0}", Resultado);
+            Console.ReadKey();
+        }
+    }
+}
