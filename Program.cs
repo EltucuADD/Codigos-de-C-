07aeb661b568fae57711dcfaadb0bb6d16ae4539
Andres Grau <119760160+EltucuADD@users.noreply.github.com>
2024-11-01 02:00:05 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _3__1
+namespace _3__2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int Numero, Resultado = 0;
+            Double Celsius, Fahrenheit = 0;
 
-            Console.WriteLine("Ingrese un numero entero");
-            Numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese los grados Celsius");
+            Celsius = Convert.ToDouble(Console.ReadLine());
 
-            Resultado = Numero * 2;
+            Fahrenheit = Celsius*(33.8);
 
-            Console.WriteLine("El resultado es: {0}", Resultado);
+            Console.WriteLine("La temperatura en grados Fahrenheit es: {0}", Fahrenheit);
             Console.ReadKey();
         }
     }
