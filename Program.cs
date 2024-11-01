b44375cc2d766a03e8922fe325014a2e37c5459e
Andres Grau <119760160+EltucuADD@users.noreply.github.com>
2024-11-01 02:04:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Celsius, Fahrenheit = 0;
+
+            Console.WriteLine("Ingrese los grados Celsius");
+            Celsius = Convert.ToDouble(Console.ReadLine());
+
+            Fahrenheit = Celsius*(33.8);
+
+            Console.WriteLine("La temperatura en grados Fahrenheit es: {0}", Fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
